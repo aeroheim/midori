a9a9dc049bde7b3518c7abd4ba15fd7d4baa6a46
Ben Pang <ben.pang@ni.com>
2019-10-08 16:17:34 -0500

    add @babel/plugin-transform-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
                 '@babel/preset-env',
               ],
               plugins: [
+                '@babel/plugin-transform-runtime',
                 '@babel/plugin-proposal-object-rest-spread',
                 '@babel/plugin-proposal-class-properties',
               ],
