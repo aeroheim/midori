f734023ca984c6562d90ab5d1949c348155b94fd
aeroheim <benben123213@gmail.com>
2019-10-21 00:02:52 -0500

    work cross fade transition

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,9 @@
 import * as three from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 import TWEEN from '@tweenjs/tween.js';
+import { CrossFadeShader } from './shaders/cross-fade';
 import Camera from './camera';
 import Background from './background';
 
@@ -9,13 +13,12 @@ class Renderer {
   _height;
 
   _renderer;
+  _composer;
+  _renderPass;
+  _fadePass;
 
-  _canvasBackground;
-  _canvasCamera;
-
-  _primaryBackground; // need a default background scene when nothing is loaded
-  _primaryCamera;
-  _primaryBuffer;
+  _background;
+  _camera;
 
   _transitionBackground;
   _transitionCamera;
@@ -28,80 +31,78 @@ class Renderer {
     this._width = domElement.clientWidth;
     this._height = domElement.clientHeight;
 
+    // initialize renderer
     this._renderer = new three.WebGLRenderer();
     this._renderer.domElement.id = 'midori';
+    this._renderer.setSize(this._width, this._height);
     this._domElement.appendChild(this._renderer.domElement);
+    this.onResize = this.onResize.bind(this);
+    window.onresize = this.onResize;
 
-    // TODO figure out buffers
-    this._primaryBuffer = new three.WebGLRenderTarget(this._width, this._height);
+    // main scene and camera
+    this._background = new Background();
+    this._camera = new Camera(this._width, this._height);
+
+    // spare scene, camera, and buffer for use in transitions
     this._transitionBuffer = new three.WebGLRenderTarget(this._width, this._height);
-
-    this._canvasBackground = new Background();
-    this._canvasBackground.setSize(this._width, this._height);
-    this._canvasCamera = new Camera(this._width, this._height);
-    this._primaryBackground = new Background();
-    this._primaryCamera = new Camera(this._width, this._height);
     this._transitionBackground = new Background();
     this._transitionCamera = new Camera(this._width, this._height);
 
-    this._renderer.setSize(this._width, this._height);
-    this.onResize = this.onResize.bind(this);
-    window.onresize = this.onResize;
+    // post-processing pipeline
+    this._composer = new EffectComposer(this._renderer);
+    this._renderPass = new RenderPass(this._background.getScene(), this._camera.getCamera());
+    this._fadePass = new ShaderPass(CrossFadeShader);
+    this._composer.addPass(this._renderPass);
+    this._composer.addPass(this._fadePass);
+  }
+
+  // TODO
+  startTransition(background) {
+    console.log('start transition');
+
+    // old background and camera are used in the transition
+    this._transitionBackground = this._background;
+    this._transitionCamera = this._camera;
+
+    // set main background, re-initialize camera
+    this._background = background;
+    this._camera = new Camera(this._width, this._height);
+    this._camera.move(this._background.getPlane(), 0, 0, 1); // TODO: preserve or lead from old camera movement
+
+    // kick off transition in post-processing
+    this._renderPass.scene = this._background.getScene();
+    this._renderPass.camera = this._camera.getCamera();
+    this._fadePass.uniforms.transition.value = true;
+    this._fadePass.uniforms.fadeTexture.value = this._transitionBuffer.texture;
+  }
+
+  // TODO
+  endTransition() {
+    console.log('end transition');
+    this._fadePass.uniforms.transition.value = false;
+    this._fadePass.uniforms.fadeTexture.value = false;
+    this._transition = null;
   }
 
   onResize() {
     this._width = this._domElement.clientWidth;
     this._height = this._domElement.clientHeight;
-    this._canvasBackground.setSize(this._width, this._height);
-
-    // TODO figure out buffers
-    this._canvasCamera.setSize(this._width, this._height);
-    this._primaryBuffer.setSize(this._width, this._height);
-    this._transitionBuffer.setSize(this._width, this._height);
-
     this._renderer.setSize(this._width, this._height);
-    this._primaryCamera.setSize(this._width, this._height);
+    this._camera.setSize(this._width, this._height);
     this._transitionCamera.setSize(this._width, this._height);
+    this._transitionBuffer.setSize(this._width, this._height);
   }
 
-  // TODO
-  // need to add support for background transitions
+  // TODO: generalize transitions
   setBackground(background) {
     if (!this._transition) {
       this._transition = new TWEEN.Tween({ opacity: 1 })
         .to({ opacity: 0 }, 1000)
         .easing(TWEEN.Easing.Cubic.Out)
-        .onStart(() => {
-          console.log('new transition');
-
-          // TODO: set canvas material to shader material so we can apply crossfade shader
-          // this._canvasBackground.setMaterial();
-
-          // old background/camera becomes transition background/camera
-          this._transitionBackground = background;
-          this._transitionCamera = this._primaryCamera;
-
-          // new background becomes primary background, re-initialize camera
-          this._primaryBackground = background;
-          this._primaryCamera = new Camera(this._width, this._height); // TODO: consider cloning camera to preserve current movement?
-
-        })
-        .onComplete(() => {
-          console.log('finish transition');
-
-          // TODO: set canvas material back to regular material
-          // this._canvasBackground.setMaterial();
-          this._transition = null;
-        })
+        .onStart(() => this.startTransition(background))
+        .onUpdate((obj) => { this._fadePass.uniforms.opacity.value = obj.opacity; })
+        .onComplete(() => this.endTransition())
         .start();
-
-      /*
-      this._primaryBackground = background;
-      const object = this._primaryBackground.getThreeObject();
-      if (object) {
-        this._primaryCamera.move(object, 0, 0, 1);
-      }
-      */
 
       return true;
     }
@@ -116,27 +117,18 @@ class Renderer {
   */
 
   renderToTarget(target, background, camera) {
-    this._renderer.setRenderTarget(target); // set render target
-    this._renderer.render(background.getThreeScene(), camera.getThreeCamera());
-    this._renderer.setRenderTarget(null); // reset render target
+    this._renderer.setRenderTarget(target);
+    this._renderer.render(background.getScene(), camera.getCamera());
+    this._renderer.setRenderTarget(null);
   }
 
-  // TODO: eventually there should be an fx scene as well?
-  // we should use an EffectComposer, apply post-processing mainly to the canvas (and render targets if necessary)
   render() {
-    // TODO figure out buffers
     if (this._transition) {
       this.renderToTarget(this._transitionBuffer, this._transitionBackground, this._transitionCamera);
     }
 
-    this.renderToTarget(this._primaryBuffer, this._primaryBackground, this._primaryCamera);
-
-    // TODO: canvas background should use a custom ShaderMaterial that diffuses between _primaryBuffer and _transitionBuffer
-    // the two textures can be injected into the shader as uniforms.
-    this._canvasCamera.move(this._canvasBackground.getThreeObject(), 0, 0, 1);
-
-    this._renderer.render(this._canvasBackground.getThreeScene(), this._canvasCamera.getThreeCamera());
-    // this._renderer.render(this._primaryBackground.getThreeScene(), this._primaryCamera.getThreeCamera());
+    this._composer.render();
+    // this._renderer.render(this._primaryBackground.getScene(), this._primaryCamera.getCamera());
   }
 }
 
