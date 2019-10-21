524e3a2eba595a51f88a59aefa0a5fcb796aaf72
aeroheim <benben123213@gmail.com>
2019-10-21 00:02:24 -0500

    add CrossFadeShader

--- src/shaders/cross-fade.js
+++ src/shaders/cross-fade.js
@@ -6,36 +6,46 @@
 
 const CrossFadeShader = {
   uniforms: {
-    tDiffuse1: { value: null },
-    tDiffuse2: { value: null },
-    mixRatio: { value: null },
+    tDiffuse: { value: null },
+    fadeTexture: { value: null },
     opacity: { value: 1.0 },
+    transition: { value: false },
   },
+
   vertexShader: [
+
     'varying vec2 vUv;',
 
     'void main() {',
-
     ' vUv = uv;',
     ' gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-
     '}',
+
   ].join('\n'),
+
   fragmentShader: [
 
     'uniform float opacity;',
-
     'uniform sampler2D tDiffuse;',
-
+    'uniform sampler2D fadeTexture;',
+    'uniform bool transition;',
     'varying vec2 vUv;',
 
     'void main() {',
-
     ' vec4 texel = texture2D( tDiffuse, vUv );',
-    ' gl_FragColor = opacity * texel;',
-
+    ' if (transition) {',
+    '   vec4 fadeTexel = texture2D( fadeTexture, vUv );',
+    '   gl_FragColor = mix( texel, fadeTexel, opacity );',
+    ' } else {',
+    '   gl_FragColor = texel;',
+    ' }',
     '}',
+
   ].join('\n'),
+};
+
+export {
+  CrossFadeShader,
 };
 
 export default CrossFadeShader;
