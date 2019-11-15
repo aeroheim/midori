b6765987c126834cfe5c94aa4b02a7d07d40dfa0
aeroheim <benben123213@gmail.com>
2019-11-14 19:10:31 -0600

    direction support for WipeShader

--- src/postprocessing/shaders/wipe.js
+++ src/postprocessing/shaders/wipe.js
@@ -5,6 +5,8 @@
 const WipeDirection = Object.freeze({
   LEFT: 0,
   RIGHT: 1,
+  TOP: 2,
+  BOTTOM: 3,
 });
 
 const WipeShader = {
@@ -13,7 +15,7 @@ const WipeShader = {
     tDiffuse2: { value: null },
     wipe: { value: 0.0 }, // a value from 0 to 1 indicating the ratio of the texture wipe
     gradient: { value: 0.0 }, // an value from 0 to 1 indicating the size of the blend gradient
-    // direction
+    direction: { value: WipeDirection.RIGHT },
     // angle
   },
 
@@ -34,6 +36,7 @@ const WipeShader = {
     'uniform sampler2D tDiffuse2;',
     'uniform float wipe;',
     'uniform float gradient;',
+    'uniform int direction;',
     'varying vec2 vUv;',
 
     'void main() {',
@@ -43,10 +46,26 @@ const WipeShader = {
     ' float wipeOffset = -gradient + (1.0 + gradient) * wipe;',
     ' float gradientOffset = wipeOffset + gradient;',
 
-    ' if (vUv.x <= wipeOffset) {',
+    ' float position;',
+
+    ' if (direction == 0) {',
+    // WipeDirection.LEFT
+    '   position = 1.0 - vUv.x;',
+    ' } else if (direction == 1) {',
+    // WipeDirection.RIGHT
+    '   position = vUv.x;',
+    ' } else if (direction == 2) {',
+    // WipeDirection.TOP
+    '   position = vUv.y;',
+    ' } else if (direction == 3) {',
+    // WipeDirection.BOTTOM
+    '   position = 1.0 - vUv.y;',
+    ' }',
+
+    ' if (position <= wipeOffset) {',
     '   gl_FragColor = texel2;',
-    ' } else if (vUv.x <= gradientOffset) {',
-    '   gl_FragColor = mix(texel2, texel1, (vUv.x - wipeOffset) / gradient);',
+    ' } else if (position <= gradientOffset) {',
+    '   gl_FragColor = mix(texel2, texel1, (position - wipeOffset) / gradient);',
     ' } else {',
     '   gl_FragColor = texel1;',
     ' }',
