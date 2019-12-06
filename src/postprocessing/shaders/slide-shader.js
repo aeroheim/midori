a30ad0061a8af5c5e1518614137a6717c34e0d01
aeroheim <benben123213@gmail.com>
2019-12-06 09:42:41 -0600

    basic horizontal slide, no blur yet

--- src/postprocessing/shaders/slide-shader.js
+++ src/postprocessing/shaders/slide-shader.js
@@ -7,7 +7,9 @@ const SlideShader = {
     tDiffuse1: { value: null },
     tDiffuse2: { value: null },
     blend: { value: 0.0 },
+    // a positive value indicating the amount to slide
     amount: { value: 0.0 },
+    // a positive value that affects the intensity of the blur
     intensity: { value: 0.0 },
   },
 
@@ -28,12 +30,16 @@ const SlideShader = {
     'uniform sampler2D tDiffuse2;',
     'uniform float blend;',
     'uniform float amount;',
+    'uniform float intensity;',
     'varying vec2 vUv;',
 
     'void main() {',
     ' vec4 texel1 = texture2D(tDiffuse1, vUv);',
     ' vec4 texel2 = texture2D(tDiffuse2, vUv);',
-    ' gl_FragColor = mix(texel1, texel2, amount);',
+    ' vec2 position = vUv;',
+    ' position.x = abs((position.x + amount) - 1.0) < .000001 ? 1.0 : (position.x + amount) - (1.0 * floor((position.x + amount) / 1.0));',
+    ' gl_FragColor = texture2D(tDiffuse2, position);',
+    // ' gl_FragColor = mix(texel1, texel2, amount);',
     '}',
 
   ].join('\n'),
