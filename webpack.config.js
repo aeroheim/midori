1b737cceaf7bebcaf7c74e51e58d4f6643333512
aeroheim <benben123213@gmail.com>
2020-02-22 12:19:15 -0600

    three.js tree shaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const path = require('path');
+const ThreeMinifierPlugin = require('@yushijinhun/three-minifier-webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
+const threeMinifier = new ThreeMinifierPlugin();
+module.exports = (env, { mode = 'development' }) => ({
+  entry: 'index.ts',
+  plugins: [
+    threeMinifier,
+    new CompressionPlugin(),
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve('build'),
+    filename: 'midori.js',
+    path: path.resolve('dist'),
     publicPath: '/',
   },
   resolve: {
+    plugins: [ threeMinifier.resolver ],
     extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
       {
+        sideEffects: false, // for tree-shaking - the codebase is pure (i.e no global logic on import)
         test: /\.(j|t)s$/,
         exclude: /node_modules/,
         use: [
@@ -39,5 +48,7 @@ module.exports = {
       },
     ],
   },
-  devtool: 'cheap-module-eval-source-map', // more accurate source map for debugging
-};
+  devtool: mode === 'development'
+    ? 'cheap-module-eval-source-map' // more accurate source map for debugging
+    : 'source-map', // original source map for production
+});
