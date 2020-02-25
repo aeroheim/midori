4f67538f110a2c3cd0cd05397c7ed969efaf0e9c
aeroheim <benben123213@gmail.com>
2020-02-25 17:39:40 -0600

    additional exports

--- src/midori.ts
+++ src/midori.ts
@@ -1,1 +1,7 @@
+
 export { BackgroundRenderer, loadImage, isWebGLSupported } from './background-renderer';
+export { TransitionType } from './pipeline/transition-pass';
+export { EffectType } from '../src/effects/effect';
+export { SlideDirection } from '../src/effects/shaders/transition/slide-shader';
+export { WipeDirection } from '../src/effects/shaders/transition/wipe-shader';
+export { Easings } from './transition';
